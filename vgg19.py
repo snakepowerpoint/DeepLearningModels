fef3aa837eae10dde474f4a5831bdf1919157381
Rahul <snakepowerpoint@gmail.com>
2019-07-25 00:18:42 +0800

    initial commit

---
+++ vgg19.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+
+
+def vgg19(inputs):
+    # block 1
+    
+    # block 2
+    # block 3
+    # block 4
+    # block 5
+    
+    return None
+
+
+def weight_variable(shape):
+    """weight_variable generates a weight variable of a given shape."""
+    initializer = tf.contrib.layers.xavier_initializer()
+    return tf.Variable(initializer(shape))
+
+
+def main():
+    return None
+
+
+if __name__ == '__main__':
+    main()
